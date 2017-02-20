37de61d443115e1eb79aa403c9366f507af68540
eabowman <eabowman@email.arizona.edu>
2017-02-20 11:21:27 -0700

    added email to script

--- scripts/dbMEM.R
+++ scripts/dbMEM.R
@@ -1,6 +1,7 @@
 ## Script created by Liz Bowman, 2017-02-15 for use in a workshop on how to examine
 ## data for spatial paterns and to test for correlations between communities
 ## and environmental variables
+## eabowman@email.arizona.edu
 
 #=========================================================================================
 # Load packages and data
